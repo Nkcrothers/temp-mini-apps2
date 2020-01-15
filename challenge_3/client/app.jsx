5fd5754c741d82f4af1c38bc40d4f1a83a527e36
Neil Crothers <nkcrothers@gmail.com>
2020-01-15 12:33:07 -0700

    mid day commit

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ScoreCard from './components/scoreCard.jsx'
 
 class App extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      value: 0,
+      input: 0
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log('Input value: ' + this.state.value);
+    this.setState({input: this.state.value});
+    event.preventDefault();
   }
 
   render(){
       return(
         <div>
-          Test
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label>What did you bowl? :</label>
+              {console.log(this.state.input)}
+              <br></br>
+              <input type='number' id='bowlingScore' name='score' required min='0' max='10' value={this.state.value} onChange={this.handleChange}></input>
+              <input type="submit" value="Submit"></input>
+            </div>
+          </form>
+          <br></br>
+          <div>
+            <ScoreCard input={this.state.input}/>
+          </div>
         </div>
       )
   }

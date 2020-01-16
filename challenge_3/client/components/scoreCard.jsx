fb71388ed7850dcb63291ce0c46891064c43d819
Neil Crothers <nkcrothers@gmail.com>
2020-01-15 17:15:19 -0700

    end of day commit

--- challenge_3/client/components/scoreCard.jsx
+++ challenge_3/client/components/scoreCard.jsx
@@ -5,14 +5,20 @@ class ScoreCard extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-
+      previousTotal: 0,
+      firstBowl: 0,
+      secondBowl: 0
     }
+  }
+
+  componentDidMount () {
+
   }
 
   render(){
     return(
       <div>
-        test
+        {console.log(this.props.value)}
       </div>
     )
   }
